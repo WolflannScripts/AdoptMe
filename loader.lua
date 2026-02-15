05a56a9df5e46f85d098df157520bf7413399599
WolflannScripts <taeistnkzxx@gmail.com>
2026-02-16 02:53:19 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjdkYmZjODA1LTE3MzktNGI5MC05MTk3LWE5YjZiMTBkMGVlNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6ImY2YjExYmUwLTY2Y2UtNDdiYS05NDEwLTJkOGY0NDM1YmUxZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
