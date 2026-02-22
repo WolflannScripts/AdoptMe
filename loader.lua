191e41e22c9606af64bb212a01d542791bf21ed6
WolflannScripts <taeistnkzxx@gmail.com>
2026-02-23 00:07:15 +0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImY2YjExYmUwLTY2Y2UtNDdiYS05NDEwLTJkOGY0NDM1YmUxZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImY2YjExYmUwLTY2Y2UtNDdiYS05NDEwLTJkOGY0NDM1YmUxZCIsImtpbmQiOiJsb2FkZXIifQ"))()
